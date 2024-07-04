c0551ccc3d4b0b60b30fb7d147b90f738cd8ee07
mhamza123 <ihamza59@icloud.com>
2024-07-04 13:17:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def printxyz():
     return 'Hello testingg'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("3002"), debug=True)
